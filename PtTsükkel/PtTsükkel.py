99eb025bd8d30f3fb47cdb1a55c42377db12daa6
opilane <opilane@HP-CZC2349HTD>
2023-01-10 09:57:10 +0200

    Add project files.

---
+++ PtTsükkel/PtTsükkel.py
@@ -0,0 +1,14 @@
+from math import * 
+from random import * 
+#Mõtle ise välja ülesanne, mis on vaja lahendada while tingimusega/while True/for kasutades. Lahenda nii while kui ka for abil.
+
+while
+
+
+
+
+#Ülesanne 9 Pisike korrutustabel
+
+n=int(input("Sisestage number n: " ))
+for i in range(1,11):
+    print(f' {5} * {i} = {n*i} ')
